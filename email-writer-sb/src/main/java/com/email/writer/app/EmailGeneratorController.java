548b49d8e4020f3d6662f542588b17624bcb216a
Varshini <varshinivenkat02@gmail.com>
2025-02-03 06:04:44 +0000

    Final Project

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -1,6 +1,4 @@
 package com.email.writer.app;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
